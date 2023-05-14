4b82f97d598fdf069e56c4d76be39203df06af47
karansingh-cs <karanbir97@outlook.com>
2023-05-14 18:03:44 -0500

    Integrated the Rest API CRUD Functionality

--- HotelBookingAPI/Program.cs
+++ HotelBookingAPI/Program.cs
@@ -1,6 +1,11 @@
+using HotelBookingAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApiContext>
+    (opt => opt.UseInMemoryDatabase("BookingDb"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
